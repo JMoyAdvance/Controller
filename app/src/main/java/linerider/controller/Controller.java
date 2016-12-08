ad8fd428f4d2a33549cca14bb9f3d80213cb210c
JMoyAdvance <jermoy1@gmail.com>
2016-12-07 18:22:33 -0600

    Committing to work away from desktop

--- app/src/main/java/linerider/controller/Controller.java
+++ app/src/main/java/linerider/controller/Controller.java
@@ -1,31 +1,39 @@
 package linerider.controller;
 
+import android.bluetooth.BluetoothSocket;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-
 import android.content.Intent;
 import android.view.View;
-
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-
+import android.widget.AdapterView;
+import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Set;
 
-public class Controller extends AppCompatActivity {
+import static android.bluetooth.BluetoothAdapter.ACTION_REQUEST_ENABLE;
 
-    Button start, left, right;
-    private BluetoothAdapter BA;
-    private Set<BluetoothDevice> pairedDevices;
+public class Controller extends AppCompatActivity
+{
+
+    Button paired, start, left, right;
+    private BluetoothAdapter BA = null;
+    private Set pairedDevices;
     ListView lv;
 
+    String address = null;
+    BluetoothSocket BS = null;
+    private boolean isConnected = false;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controller);
 
@@ -34,12 +42,84 @@ public class Controller extends AppCompatActivity {
         right = (Button)findViewById(R.id.right);
 
         BA = BluetoothAdapter.getDefaultAdapter();
-      //  lv = (ListView)findViewById(R.id.);
+        lv = (ListView)findViewById(R.id.listView);
+
+        if(BA == null)
+        {
+            Toast.makeText(getApplicationContext(), "Bluetooth Device Not Available", Toast.LENGTH_LONG).show();
+            finish();
+        }
+        else
+        {
+            if (BA.isEnabled())
+            {
+
+            }
+            else
+            {
+                Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(turnBTon, 1);
+            }
+        }
     }
 
-    public void on(View V){
-        if(!BA.isEnabled()){
-
+    paired.setOnClickListener(new View.OnClickListener()
+    {
+        public void onClick(View v)
+        {
+            pairedDevicesList();
         }
+    });
+
+
+    private void pairedDevicesList()
+    {
+        pairedDevices = BA.getBondedDevices();
+        ArrayList list = new ArrayList();
+
+        if(pairedDevices.size() > 0)
+        {
+            for(BluetoothDevice bt : pairedDevices)
+            {
+                list.add(bt.getName() + "\n" + bt.getAddress());
+            }
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "No Paired Bluetooth Devices Found", Toast.LENGTH_LONG).show();
+        }
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(myListClickListener);
+    }
+
+    private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener()
+    {
+        public void onItemClick (AdapterView av, View v, int arg2, long arg3)
+        {
+            // Get the device MAC address, the last 17 chars in the View
+            String info = ((TextView) v).getText().toString();
+            String address = info.substring(info.length() - 17);
+            // Make an intent to start next activity.
+            Intent i = new Intent(DeviceList.this, ledControl.class);
+            //Change the activity.
+            i.putExtra(EXTRA_ADDRESS, address); //this will be received at ledControl (class) Activity
+            startActivity(i);
+        }
+    };
+
+    private void start()
+    {
+
+    }
+
+    private void left()
+    {
+
+    }
+
+    private void right()
+    {
+
     }
 }
